d8a676e871a700dd767683145ec22963967d2738
ERIWUG <e.belyi2013@gmail.com>
2025-10-01 18:02:44 +0300

    V2

--- PDDVLite/PDDVLite/Components/Hubs/ChatHub.cs
+++ PDDVLite/PDDVLite/Components/Hubs/ChatHub.cs
@@ -9,6 +9,24 @@ namespace PDDVLite.Components.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task AddUserToGroup(string user, string group) {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+            await Clients.All.SendAsync("Text",$"Пользователь {user} вошел в группу {group}");
+        }
+
+        public async Task SendToGroup(string user, string group, string message)
+        {
+            await Clients.Group(group).SendAsync("TextGroup",$"Внутри группы {group}",user, message);
+        }
+
+        public async Task ValueChange(string newValue)
+        {
+            await Clients.Group("Teacher").SendAsync("StudentTask", newValue);
+        
+        }
+
+       
     }
 }
 
